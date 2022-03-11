2777be9584196f885267b81741f26114b921d8ea
avaeazadi2021 <avaeazadi2@gmail.com>
2022-03-11 19:21:53 +0200

    this is the galleries

---
+++ script.js
@@ -0,0 +1,16 @@
+function deskDesign() {
+    var element = document.getElementById("desktop");
+  
+ }
+ function inner() {
+    var element = document.getElementById("inner");
+ 
+ }
+ function TABLET() {
+    var element = document.getElementById("tablet");
+ 
+ }
+ function MOBILE() {
+    var element = document.getElementById("mobile");
+  
+ }
---
+++ testspace/script.js
@@ -0,0 +1,23 @@
+var arr=[1,2,3,4,5,6,7,8,9,10];
+var i =0;
+while (i<arr.lenght){
+    console.log(arr[i]+2000)
+    i++;
+}
+//loop over and over again
+while(true){
+console.log(" i am running");
+}  
+//how many time loops
+var i =1;
+while(i<20){
+    console.log(" counter is"+i);
+    i++;
+}
+
+
+
+var arr=['has',20,true]
+arr[1]
+20
+arr[2]
